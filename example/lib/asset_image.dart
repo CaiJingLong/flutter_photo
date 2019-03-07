c944b6ae9f4018d1469f24c1fd3cd8df86f7cb0e
Caijinglong <cjl_spy@163.com>
2019-03-07 09:50:35 +0800

    update preview to preview pick asset

---
+++ example/lib/asset_image.dart
@@ -0,0 +1,64 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
+
+class AssetImageWidget extends StatelessWidget {
+  final AssetEntity assetEntity;
+  final double width;
+  final double height;
+  final BoxFit boxFit;
+
+  const AssetImageWidget({
+    Key key,
+    @required this.assetEntity,
+    this.width,
+    this.height,
+    this.boxFit,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (assetEntity == null) {
+      return _buildContainer();
+    }
+    return FutureBuilder<Size>(
+      builder: (c, s) {
+        if (!s.hasData) {
+          return Container();
+        }
+        var size = s.data;
+        return FutureBuilder<Uint8List>(
+          builder: (BuildContext context, snapshot) {
+            if (snapshot.hasData) {
+              return _buildContainer(
+                child: Image.memory(
+                  snapshot.data,
+                  width: width,
+                  height: height,
+                  fit: boxFit,
+                ),
+              );
+            } else {
+              return _buildContainer();
+            }
+          },
+          future: assetEntity.thumbDataWithSize(
+            size.width.toInt(),
+            size.height.toInt(),
+          ),
+        );
+      },
+      future: assetEntity.size,
+    );
+  }
+
+  Widget _buildContainer({Widget child}) {
+    child ??= Container();
+    return Container(
+      width: width,
+      height: height,
+      child: child,
+    );
+  }
+}
---
+++ example/lib/preview.dart
@@ -0,0 +1,28 @@
+import 'package:example/asset_image.dart';
+import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
+
+class PreviewPage extends StatelessWidget {
+  final List<AssetEntity> list;
+
+  const PreviewPage({Key key, this.list = const []}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("预览"),
+      ),
+      body: ListView(
+        children: list
+            .map((item) => AssetImageWidget(
+                  assetEntity: item,
+                  width: 300,
+                  height: 200,
+                  boxFit: BoxFit.cover,
+                ))
+            .toList(),
+      ),
+    );
+  }
+}
