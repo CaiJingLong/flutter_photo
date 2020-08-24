a5961cfa9177d90f4628aeea9bbc36521d1e94fb
ippo012 <ippo@ginco.io>
2020-08-24 19:56:59 +0900

    Update example

--- example/android/app/src/main/java/com/example/example/MainActivity.java
+++ example/android/app/src/main/java/com/example/example/MainActivity.java
@@ -1,14 +1,10 @@
-package com.example.example;
+ package io.flutter.plugins.firebasecoreexample;
 
-import android.os.Bundle;
+ import io.flutter.embedding.android.FlutterActivity;
+ import io.flutter.embedding.engine.FlutterEngine;
+ import io.flutter.plugins.firebase.core.FirebaseCorePlugin;
 
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
-
-public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
-}
+ public class MainActivity extends FlutterActivity {
+   // You can keep this empty class or remove it. Plugins on the new embedding
+   // now automatically registers plugins.
+ }
